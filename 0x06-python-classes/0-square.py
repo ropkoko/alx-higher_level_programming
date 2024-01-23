1f9bebf3bc2f69310b207bbd569ae2517332675f
ropkoko <ropbrenda4@gmail.com>
2024-01-23 03:47:58 -0800

    classes python projects

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""Define a class square. """
 
-class square:
-    """Represent a square. """
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
     pass
